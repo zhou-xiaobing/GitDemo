695c6af0cd6b5d5e645e89977a9cd1c125613f20
zhou-xiaobing <zhouxiaobing159753@qq.com>
2019-09-24 17:49:27 +0800

    IDEA整合github测试

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @Author Zhou-Xiaobing
+ * @Date 2019/9/24 17:45
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+
+}
